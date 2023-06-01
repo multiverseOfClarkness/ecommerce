57009c2c33a9f71c8ccf97a936dbb79b136ef882
multiverseOfClarkness <clarkjames.digiteer@gmail.com>
2023-06-01 10:07:04 +0800

    Add new product, with input validation.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,24 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end  
+
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+    validates :price, presence: true, numericality: { only_float: true }
+    validates :quantity, presence: true, numericality: { only_integer: true }
 end
