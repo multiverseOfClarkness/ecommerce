6cb5c95babee2e9a21e3e2391882a02fe2a15567
multiverseOfClarkness <clarkjames.digiteer@gmail.com>
2023-06-14 20:43:39 +0800

    Changed name of Customer table to User table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/home'
   # config/routes.rb
   devise_for :admins, controllers: { sessions: 'sessions' }
-  devise_for :customers, controllers: { sessions: 'sessions' }
+  devise_for :users, controllers: { sessions: 'sessions' }
 
   
   root "home#home"
