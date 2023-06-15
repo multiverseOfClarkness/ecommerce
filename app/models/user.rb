b040a292e12733f3d5ae14b62a0443782596daa6
multiverseOfClarkness <clarkjames.digiteer@gmail.com>
2023-06-15 17:22:08 +0800

    Add role column to user table, and default role mechanism for new accounts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :set_default_role
+
+  belongs_to :role, foreign_key: :role
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  belongs_to :role
 
+  
+  private
+
+  def set_default_role
+    self.role ||= Role.second.id
+  end
 end
